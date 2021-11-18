fc370b6e08e4179009799f29249dd0e7e5c0acaa
“glen” <15529031941@163.com>
2021-11-18 23:47:59 +0800

    修改一些错误test

--- src/main/java/com/glen/lottery/LotteryApplication.java
+++ src/main/java/com/glen/lottery/LotteryApplication.java
@@ -9,7 +9,7 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
  * @author : lizi
  * @date : 2021-11-18 10:23
  **/
-@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
+@SpringBootApplication
 public class LotteryApplication {
     public static void main(String[] args) {
         SpringApplication.run(LotteryApplication.class, args);
